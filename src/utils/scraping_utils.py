4ad0361572f160f3468e45dce2ebd0db5be41c05
Jean <jtrujillo@galgo.com>
2025-12-16 12:41:13 -0500

    feat(get_all_urls_from_website): Traer todas las URLs encontradas de un sitio mediante una URL base

---
+++ src/utils/scraping_utils.py
@@ -0,0 +1,12 @@
+from firecrawl import Firecrawl
+from utils.process_firecrawl_response import get_urls_from_firecrawl_map
+
+class ScrapingUtils:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.firecrawl = Firecrawl(api_key=self.api_key)
+
+    def get_all_urls_from_website(self, url: str):
+        """ Trae todas las URLs de un sitio web """
+        url_list = self.firecrawl.map(url=url)
+        return get_urls_from_firecrawl_map(url_list)
