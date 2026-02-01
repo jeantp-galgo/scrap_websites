927374046bbb9f7868eeeeeb6c603d2e1df364d8
Jean <jtrujillo@galgo.com>
2026-01-31 23:13:16 -0500

    feat: funciones para traer contenido de una web y todas las urls

---
+++ src/core/scraper/app.py
@@ -0,0 +1,18 @@
+from firecrawl import Firecrawl
+import os
+from src.core.scraper.utils import get_urls_from_firecrawl_map
+
+class ScrapingUtils:
+    def __init__(self):
+        self.api_key = os.getenv("FIRECRAWL_API_KEY")
+        self.firecrawl = Firecrawl(api_key=self.api_key)
+
+    def get_content_from_website(self, url: str, formats: list = []):
+        """ Trae contenido de la web según el formato dado """
+        doc = self.firecrawl.scrape(url=url, formats=formats)
+        return doc
+
+    def get_all_urls_from_website(self, url: str):
+        """ Trae todas las URLs de un sitio web """
+        url_list = self.firecrawl.map(url=url)
+        return get_urls_from_firecrawl_map(url_list)
