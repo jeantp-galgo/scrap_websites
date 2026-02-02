17a321ac72de30df816b50985dfc132523d69809
Jean <jtrujillo@galgo.com>
2026-02-02 15:17:51 -0500

    update: se mueve la carpeta brands afuera de images

--- src/core/scraper/images/brands/italika/handle.py
+++
@@ -1,5 +0,0 @@
-from src.core.scraper.images.brands.italika.utils import extract_main_images
-# *: Las URLs de interés son aquellas que tienen "width" o "height" incluída.
-
-def handle_italika(extracted_images_list):
-    return extract_main_images(extracted_images_list)
--- src/core/scraper/images/brands/italika/utils.py
+++
@@ -1,7 +0,0 @@
-
-def extract_main_images(images_list: list[str]):
-    main_images = []
-    for images in images_list:
-        if "width" in images:
-            main_images.append(images)
-    return main_images
